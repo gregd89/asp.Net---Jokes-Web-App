6f9292c423ddc97023a9fbc4dae63b4c5400750f
Greg Dunne <gregd89@users.noreply.github.com>
2026-02-13 09:42:52 +0000

    initial commit

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public int JokeQuestion { get; set; }
+
+        public int JokeAnswer { get; set; }
+
+        // Constructor
+        public Joke()
+        {
+
+        }
+    }
+}
